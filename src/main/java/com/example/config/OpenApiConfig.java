2c1d10d6b8b970eacb5457bac2649d3320d5473e
Mana-ay <richmund.l.mana-ay@accenture.com>
2025-12-01 16:52:01 +0800

    Update cors

--- src/main/java/com/example/config/OpenApiConfig.java
+++ src/main/java/com/example/config/OpenApiConfig.java
@@ -1,5 +1,7 @@
 package com.example.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -7,6 +9,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://demo-vat-id-service-production.up.railway.app")
+        }
+)
 public class OpenApiConfig {
 
     @Bean
