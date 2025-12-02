90226af8cfbee0b09e8feda0cee9acc3e74b84db
Mana-ay <richmund.l.mana-ay@accenture.com>
2025-12-02 12:22:28 +0800

    logging

---
+++ src/main/java/com/example/config/RedisLoggingConfig.java
@@ -0,0 +1,36 @@
+package com.example.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+import java.util.logging.Logger;
+
+@Configuration
+@Slf4j
+public class RedisLoggingConfig {
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        String host = System.getenv("SPRING_DATA_REDIS_HOST");
+        String port = System.getenv("SPRING_DATA_REDIS_PORT");
+        String password = System.getenv("REDIS_PASSWORD");
+        boolean ssl = Boolean.parseBoolean(System.getenv("SPRING_DATA_REDIS_SSL_ENABLED"));
+
+        System.out.println("🚀 Connecting to Redis: {}:{} (SSL: {})" +
+                host + ":" + port + ":" + ssl);
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(Integer.parseInt(port));
+        config.setPassword(password);
+
+        LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(config);
+        connectionFactory.setUseSsl(ssl);
+
+        return connectionFactory;
+    }
+}
