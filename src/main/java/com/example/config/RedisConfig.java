d17530b35919031473665814518db94bd206bba4
Mana-ay <richmund.l.mana-ay@accenture.com>
2025-12-02 12:29:05 +0800

    Update redis config

--- src/main/java/com/example/config/RedisConfig.java
+++ src/main/java/com/example/config/RedisConfig.java
@@ -10,11 +10,6 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
-    }
-
-    @Bean
     public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
