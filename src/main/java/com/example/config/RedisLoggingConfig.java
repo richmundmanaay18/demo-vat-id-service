b28fb6bd804077c6fef6cc83f3e216b5e9f61408
Mana-ay <richmund.l.mana-ay@accenture.com>
2025-12-02 13:56:19 +0800

    Update redis config

--- src/main/java/com/example/config/RedisLoggingConfig.java
+++ src/main/java/com/example/config/RedisLoggingConfig.java
@@ -1,28 +1,19 @@
 package com.example.config;
 
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-
-import java.util.logging.Logger;
 
 @Configuration
 public class RedisLoggingConfig {
 
-    @Autowired
-    private Environment env;
-
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        String host = env.getProperty("SPRING_DATA_REDIS_HOST");
-        String port = env.getProperty("SPRING_DATA_REDIS_PORT");
-        String password = env.getProperty("REDIS_PASSWORD");
-        boolean ssl = Boolean.parseBoolean(env.getProperty("SPRING_DATA_REDIS_SSL_ENABLED"));
+        String host = System.getenv("REDIS_HOST");
+        String port = System.getenv("REDIS_PORT");
+        String password = System.getenv("REDIS_PASSWORD");
+        boolean ssl = Boolean.parseBoolean(System.getenv("SPRING_DATA_REDIS_SSL_ENABLED"));
 
         System.out.println("🚀 Connecting to Redis: {}:{} (SSL: {})" +
                 host + ":" + port + ":" + ssl);
